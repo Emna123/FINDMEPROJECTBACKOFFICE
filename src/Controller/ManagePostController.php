3c1e20a242bb9acdb73b5ad1744e4d1f2be395d3
Mehdi <47383179+mehdidm@users.noreply.github.com>
2020-11-26 15:26:20 +0100

    Posts management interface

---
+++ src/Controller/ManagePostController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ManagePostController extends AbstractController
+{
+    /**
+     * @Route("/manage/post", name="manage_post")
+     */
+    public function index(): Response
+    {
+        return $this->render('manage_post/index.html.twig', [
+            'controller_name' => 'ManagePostController',
+        ]);
+    }
+}
